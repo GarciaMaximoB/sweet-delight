91ec2afc2e3cc6b1cd20fc923e97d5d946960b2f
Maximo Bautista Garcia <maximogarciadev@gmail.com>
2024-09-15 17:16:48 -0300

    feat: Creando rutas

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,20 @@
 import "./App.css";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import HomePage from "./pages/HomePage";
+import Cart from "./pages/Cart.jsx";
+import About from "./pages/About";
+import Products from "./pages/Products";
 
 function App() {
   return (
-    <>
-      <h1>Sweet Delight</h1>
-    </>
+    <Router>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/cart" element={<Cart />} />
+        <Route path="/products" element={<Products />} />
+      </Routes>
+    </Router>
   );
 }
 
