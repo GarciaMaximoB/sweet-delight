8d3b2c27ba1e9fdfe4876012c79afd5ad44bad05
Maximo Bautista Garcia <maximogarciadev@gmail.com>
2024-09-19 14:52:59 -0300

    fix: import app css on navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,6 +1,6 @@
 import { Link } from "react-router-dom";
 import { links } from "../utils/links";
-import "../app.css";
+import "../App.css";
 import logo from "../assets/logo.jpeg";
 
 export default function NavBar() {
